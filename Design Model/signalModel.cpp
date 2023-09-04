2df9112aec60f1c8784a25536aaf26dd8504d2a9
32943@Gao <32943@Gao>
2023-09-04 20:34:26 +0800

    单例懒汉模式

---
+++ Design Model/signalModel.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class SingleInstance{
+public:
+    static SingleInstance* getInstance(){
+        static SingleInstance ins;
+        return &ins;
+    }
+    ~SingleInstance(){};
+
+private:
+    SingleInstance(){ cout << "SingleInstance() 饿汉" << endl; }
+    SingleInstance(const SingleInstance& other){};
+    SingleInstance& operator=(const SingleInstance& other){return *this;}
+};
+
+int main(){
+    SingleInstance* ins = SingleInstance::getInstance();
+    return 0;
+}
